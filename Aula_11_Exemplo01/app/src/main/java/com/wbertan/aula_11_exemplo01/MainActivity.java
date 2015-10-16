bfba71cfe81752a58c35f6b41ee36b775a109adc
Diego Motta <diegomottas@hotmail.com>
2015-10-15 21:15:19 -0300

    selectors

--- Aula_11_Exemplo01/app/src/main/java/com/wbertan/aula_11_exemplo01/MainActivity.java
+++ Aula_11_Exemplo01/app/src/main/java/com/wbertan/aula_11_exemplo01/MainActivity.java
@@ -2,15 +2,31 @@ package com.wbertan.aula_11_exemplo01;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Neste exemplo as partes mais importantes são os resources de drawable...
+        listView = (ListView) findViewById(R.id.listView);
+        String[] newsFeed = getResources().getStringArray(R.array.url_authentication);
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.row, R.id.url, newsFeed));
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setSelected(true);
+                return true;
+            }
+        });
     }
 }
